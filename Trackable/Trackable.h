8618ce89e77446818d429100d94c3ede8e206b68
Vojta Stavik <vojta@strv.com>
2015-12-06 18:09:19 +0100

    Initial Commit

---
+++ Trackable/Trackable.h
@@ -0,0 +1,19 @@
+//
+//  Trackable.h
+//  Trackable
+//
+//  Created by Vojta Stavik on 06/12/15.
+//  Copyright © 2015 STRV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Trackable.
+FOUNDATION_EXPORT double TrackableVersionNumber;
+
+//! Project version string for Trackable.
+FOUNDATION_EXPORT const unsigned char TrackableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Trackable/PublicHeader.h>
+
+
