b4046214ff12b440ffcb54ae4f79ca3bb0c78f01
Vojta Stavik <vojta@strv.com>
2015-12-06 21:02:39 +0100

    updateValueFrom tests

--- TrackableTests/Key-Tests.swift
+++ TrackableTests/Key-Tests.swift
@@ -11,22 +11,23 @@ import Foundation
 import Quick
 import Nimble
 
-enum TestKeys : String, Key {
-    enum Tests : String, Key {
-        case test1
-        case test2
-    }
-    case test1
-}
-
 class KeyTests : QuickSpec {
+
+    enum TestKeys : String, Key {
+        enum Tests : String, Key {
+            case test1
+            case test2
+        }
+        case test1
+    }
+    
     override func spec() {
         describe("Key") {
             context("description") {
                 it("should be generated based on namespace structure") {
-                    expect(TestKeys.Tests.test1.description).to(equal("TrackableTests.TestKeys.Tests.test1"))
-                    expect(TestKeys.Tests.test2.description).to(equal("TrackableTests.TestKeys.Tests.test2"))
-                    expect(TestKeys.test1.description).to(equal("TrackableTests.TestKeys.test1"))
+                    expect(TestKeys.Tests.test1.description).to(equal("TrackableTests.KeyTests.TestKeys.Tests.test1"))
+                    expect(TestKeys.Tests.test2.description).to(equal("TrackableTests.KeyTests.TestKeys.Tests.test2"))
+                    expect(TestKeys.test1.description).to(equal("TrackableTests.KeyTests.TestKeys.test1"))
                 }
             }
         }
---
+++ TrackableTests/Property-Tests.swift
@@ -0,0 +1,74 @@
+//
+//  Property-Tests.swift
+//  Trackable
+//
+//  Created by Vojta Stavik on 06/12/15.
+//  Copyright © 2015 STRV. All rights reserved.
+//
+
+import Foundation
+@testable import Trackable
+import Quick
+import Nimble
+
+class PropertyTests : QuickSpec {
+
+    enum TestKeys : String, Key {
+        case value1
+        case value2
+        case value3
+    }
+
+    override func spec() {
+        describe("Set of properties") {
+            context("when called updateValueFrom") {
+                it("should add new value if the old one isn't in the set") {
+                    var testSet = Set<TrackedProperty>([TestKeys.value1 ~>> "value1old" , TestKeys.value2 ~>> "value2old"])
+                    let newValue = TestKeys.value3 ~>> "value3new"
+                    
+                    testSet.updateValuesFrom(Set<TrackedProperty>([newValue]))
+                    
+                    expect(testSet.count).to(equal(3))
+                    expect(testSet.filter{ $0.key == TestKeys.value1.description }).toNot(beEmpty())
+                    expect(testSet.filter{ $0.key == TestKeys.value2.description }).toNot(beEmpty())
+                    expect(testSet.filter{ $0.key == TestKeys.value3.description }).toNot(beEmpty())
+                    testSet.forEach {
+                        switch $0.key {
+                        case TestKeys.value1.description:
+                            expect( $0.value as? String ).to(equal("value1old"))
+                        case TestKeys.value2.description:
+                            expect( $0.value as? String ).to(equal("value2old"))
+                        case TestKeys.value3.description:
+                            expect( $0.value as? String ).to(equal("value3new"))
+                        default:
+                            fail()
+                        }
+                    }
+                }
+
+                it("should update value if called with the existing") {
+                    var testSet = Set<TrackedProperty>([TestKeys.value1 ~>> "value1old" , TestKeys.value2 ~>> "value2old"])
+                    let newValue = TestKeys.value2 ~>> "value2new"
+                    
+                    testSet.updateValuesFrom(Set<TrackedProperty>([newValue]))
+                    
+                    expect(testSet.count).to(equal(2))
+                    expect(testSet.filter{ $0.key == TestKeys.value1.description }).toNot(beEmpty())
+                    expect(testSet.filter{ $0.key == TestKeys.value2.description }).toNot(beEmpty())
+                    expect(testSet.filter{ $0.key == TestKeys.value3.description }).to(beEmpty())
+                    testSet.forEach {
+                        switch $0.key {
+                        case TestKeys.value1.description:
+                            expect( $0.value as? String ).to(equal("value1old"))
+                        case TestKeys.value2.description:
+                            expect( $0.value as? String ).to(equal("value2new"))
+                        default:
+                            fail()
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
